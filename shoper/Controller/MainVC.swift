96a58cc7d5a07b27ef852e18d711058b05002888
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-09-12 19:42:52 +0300

    Added Calculation Logic. Added custom label with currency icon, depending on user location.

--- shoper/Controller/MainVC.swift
+++ shoper/Controller/MainVC.swift
@@ -14,6 +14,8 @@ class MainVC: UIViewController {
 
     @IBOutlet weak var wageTxt: CurrencyTextField!
     @IBOutlet weak var priceTxt: CurrencyTextField!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var hoursLbl: UILabel!
     
     //MARK: Life cycle
     
@@ -35,11 +37,36 @@ class MainVC: UIViewController {
         
         wageTxt.inputAccessoryView = calcBtn
         priceTxt.inputAccessoryView = calcBtn
+        
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
     }
     
     @objc func calculate() {
-        print("we got here")
+        if let wageTxt = wageTxt.text, let priceTxt = priceTxt.text {
+            if let wage = Double(wageTxt), let price = Double(priceTxt) {
+                view.endEditing(true)
+                resultLbl.isHidden = false
+                hoursLbl.isHidden = false
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
     }
+    
+    func resetAppState() {
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+        wageTxt.text = ""
+        priceTxt.text = ""
+    }
+    
+    //MARK: IBActions
 
+    @IBAction func clearCalculatorPressed(_ sender: UIButton) {
+        
+        resetAppState()
+        
+    }
+    
 }
 
